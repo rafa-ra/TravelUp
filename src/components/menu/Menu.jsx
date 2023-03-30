0e0ba2a44850b172b354eb30b58f49af56f5831f
Rafael <reis.rafael18@gmail.com>
2023-03-29 22:20:38 -0300

    feat: menu lacking icons

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -22,52 +22,105 @@ export const Menu = () => {
 
   return (
     <div className={`menu-container ${closedMenuClass}`} styles={styles}>
+      <div className="menu-icon-container">
+        <div className="bckg-div">
+          <img src={menuImg} className="menu-icon" onClick={menuToggle}></img>
+        </div>
+      </div>
       {!closedMenu ? (
         <>
-          <div className="menu-icon-container">
-            <div className="bckg-div">
-              <img
-                src={menuImg}
-                className="menu-icon"
-                onClick={menuToggle}
-              ></img>
+          <div className="menu-content">
+            <div>
+              {textContent.components.menu.items.map(
+                (item, index) =>
+                  !item.aboutItem && (
+                    <div
+                      onClick={() => handleClick(index)}
+                      className={`menu-item ${
+                        index === activeItem ? "active" : ""
+                      }`}
+                    >
+                      <div className="item-content">
+                        <img src={icon} className="item-icon"></img>
+                        <p>{item.language[language]}</p>
+                      </div>
+                      {index === activeItem && (
+                        <div className="active-item-bar"> </div>
+                      )}
+                    </div>
+                  )
+              )}
+            </div>
+            <div className="about">
+              <h5 className="submenu-title">
+                {textContent.components.menu.title.language[language]}
+              </h5>
+              {textContent.components.menu.items.map(
+                (item, index) =>
+                  item.aboutItem && (
+                    <div
+                      onClick={() => handleClick(index)}
+                      className={`menu-item ${
+                        index === activeItem ? "active" : ""
+                      }`}
+                    >
+                      <div className="item-content">
+                        <img src={icon} className="item-icon"></img>
+                        <p>{item.language[language]}</p>
+                      </div>
+                      {index === activeItem && (
+                        <div className="active-item-bar"> </div>
+                      )}
+                    </div>
+                  )
+              )}
             </div>
           </div>
-          {textContent.components.menu.map((item, index) => (
-            <div
-              onClick={() => handleClick(index)}
-              className={`menu-item ${index === activeItem ? "active" : ""}`}
-            >
-              <div className="item-content">
-                <img src={icon} className="item-icon"></img>
-                <p>{item.language[language]}</p>
-              </div>
-              {index === activeItem && <div className="active-item-bar"> </div>}
-            </div>
-          ))}
         </>
       ) : (
         <>
-          <div className="menu-icon-container">
-            <div className="bckg-div">
-              <img
-                src={menuImg}
-                className="menu-icon"
-                onClick={menuToggle}
-              ></img>
+          <div className="menu-content">
+            <div>
+              {textContent.components.menu.items.map(
+                (item, index) =>
+                  !item.aboutItem && (
+                    <div
+                      onClick={() => handleClick(index)}
+                      className={`menu-item ${
+                        index === activeItem ? "active" : ""
+                      }`}
+                    >
+                      <div className="item-content">
+                        <img src={icon} className="item-icon"></img>
+                      </div>
+                      {index === activeItem && (
+                        <div className="active-item-bar"> </div>
+                      )}
+                    </div>
+                  )
+              )}
+            </div>
+            <div className="about">
+              {textContent.components.menu.items.map(
+                (item, index) =>
+                  item.aboutItem && (
+                    <div
+                      onClick={() => handleClick(index)}
+                      className={`menu-item ${
+                        index === activeItem ? "active" : ""
+                      }`}
+                    >
+                      <div className="item-content">
+                        <img src={icon} className="item-icon"></img>
+                      </div>
+                      {index === activeItem && (
+                        <div className="active-item-bar"> </div>
+                      )}
+                    </div>
+                  )
+              )}
             </div>
           </div>
-          {textContent.components.menu.map((item, index) => (
-            <div
-              onClick={() => handleClick(index)}
-              className={`menu-item ${index === activeItem ? "active" : ""}`}
-            >
-              <div className="item-content">
-                <img src={icon} className="item-icon"></img>
-              </div>
-              {index === activeItem && <div className="active-item-bar"> </div>}
-            </div>
-          ))}
         </>
       )}
     </div>
