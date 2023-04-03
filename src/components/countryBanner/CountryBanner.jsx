f259656583fdb50f4832db3a62472b592887dbda
Rafael <reis.rafael18@gmail.com>
2023-04-03 16:26:48 -0300

    countries container finished

--- src/components/countryBanner/CountryBanner.jsx
+++ src/components/countryBanner/CountryBanner.jsx
@@ -2,67 +2,40 @@ import React, { useState } from "react";
 import styles from "./CountryBanner.scss";
 
 const CountryBanner = ({ countryInfo, count }) => {
-  const [visibleFlipDiv, setVisibleFlipDiv] = useState(false);
-
-  const hiddenClass = visibleFlipDiv ? "visible" : "";
-
   return (
     <>
       {count % 2 === 0 ? (
-        <div
-          className="country-banner"
-          onMouseEnter={() => {
-            setVisibleFlipDiv(!visibleFlipDiv);
-            console.log(visibleFlipDiv);
-            return;
-          }}
-          onMouseLeave={() => {
-            setVisibleFlipDiv(!visibleFlipDiv);
-            return console.log(visibleFlipDiv);
-          }}
-          styles={styles}
-        >
+        <div className="country-banner">
           <img
             src={countryInfo.flag}
             className="country-flag even-banner-flag"
             styles={styles}
           ></img>
-          <div className={`flip-banner ${hiddenClass}`} styles={styles}>
-            <h4>HELLO</h4>
+          <div
+            className={`flip-banner ${countryInfo.classname}`}
+            styles={styles}
+          >
+            <h4>{countryInfo.country}</h4>
           </div>
           <img
             src={countryInfo.picture}
             className="country-pic"
             styles={styles}
           ></img>
-          <div className="country-name" styles={styles}>
-            {countryInfo.country}
-          </div>
         </div>
       ) : (
-        <div
-          className="country-banner"
-          onMouseEnter={() => {
-            setVisibleFlipDiv(!visibleFlipDiv);
-            return console.log(visibleFlipDiv);
-          }}
-          onMouseLeave={() => {
-            setVisibleFlipDiv(!visibleFlipDiv);
-            return console.log(visibleFlipDiv);
-          }}
-          styles={styles}
-        >
-          <div className={`flip-banner ${hiddenClass}`} styles={styles}>
-            <h4>HELLO</h4>
+        <div className="country-banner" styles={styles}>
+          <div
+            className={`flip-banner ${countryInfo.classname}`}
+            styles={styles}
+          >
+            <h4>{countryInfo.country}</h4>
           </div>
           <img
             src={countryInfo.flag}
             className="country-flag odd-banner-flag"
             styles={styles}
           ></img>
-          <div className="country-name" styles={styles}>
-            {countryInfo.country}
-          </div>
           <img
             src={countryInfo.picture}
             className="country-pic"
