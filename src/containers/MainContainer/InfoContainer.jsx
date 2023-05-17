5cc3120bd8e5efaedcb5842f34e4b7092e146e75
Rafael <reis.rafael18@gmail.com>
2023-05-17 17:55:52 -0300

    feat: infoContainer jsx

--- src/containers/MainContainer/InfoContainer.jsx
+++ src/containers/MainContainer/InfoContainer.jsx
@@ -1,8 +1,44 @@
-import React from "react";
-import styles from "./MainContainer.scss";
+import React, { useContext, useEffect, useState } from "react";
+import styles from "./InfoContainer.scss";
+
+import NavigationContext from "../../context/NavigationContext";
+import LanguageContext from "../../context/LanguageContext";
+import { menuContent } from "../../assets/data";
 
 const InfoContainer = () => {
-  return <div className="info-container" styles={styles}></div>;
+  const { currentPath } = useContext(NavigationContext);
+  const { language } = useContext(LanguageContext);
+  const [information, setInformation] = useState([]);
+
+  useEffect(() => {
+    if (currentPath) {
+      const filteredInfo = menuContent.items.filter(
+        (e) => e.path === currentPath
+      );
+      setInformation(filteredInfo[0].language[language]);
+    }
+  }, [currentPath, language]);
+
+  return (
+    <div className="info-container" styles={styles}>
+      <div className="title-div">
+        <h2 className="container-title">Info Container:</h2>
+        <h3 className="reference-container">{information}</h3>
+      </div>
+      <div className="content">
+        <div className="tech-div">
+          <h3 className="content-title">Technologies</h3>
+          <div>Hi</div>
+        </div>
+        <div className="challenges-div">
+          <h3 className="content-title">Challenges</h3>
+          <textarea className="challenges-description">
+            Example Description
+          </textarea>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default InfoContainer;
